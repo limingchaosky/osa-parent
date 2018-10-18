bb5ef2dde45ff630f97dffa2be4bc2d1d6f82d7d
limingchaosky <limingchaosky@163.com>
2018-10-18 11:21:04 +0800

    1.将security工具类添加到容器中，静态方法中调用容器查找用户。

--- osa-core/src/main/java/com/goldencis/osa/core/utils/SecurityUtils.java
+++ osa-core/src/main/java/com/goldencis/osa/core/utils/SecurityUtils.java
@@ -1,20 +1,38 @@
 package com.goldencis.osa.core.utils;
 
+import com.goldencis.osa.core.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by limingchao on 2018/10/13.
  */
+@Component
 public class SecurityUtils {
 
-    public static String getCurrentUser() {
+    private static SecurityUtils securityUtils;
+
+    @Autowired
+    private IUserService userService;
+
+    @PostConstruct
+    public void init() {
+        securityUtils = this;
+        securityUtils.userService = this.userService;
+    }
+
+    public static com.goldencis.osa.core.entity.User getCurrentUser() {
         SecurityContext securityContext = SecurityContextHolder.getContext();
         Authentication authentication = securityContext.getAuthentication();
         if (authentication != null && authentication.getPrincipal() instanceof User) {
-            return ((User) authentication.getPrincipal()).getUsername();
+            String username = ((User) authentication.getPrincipal()).getUsername();
+            return securityUtils.userService.findUserByUserName(username);
         } else {
             return null;
         }
